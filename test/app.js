49aba4fbc0bf8fdac84ca2d98aa3c716c8324505
DESKTOP-KO32EAR\icimi <icimidemirag@gmail.com>
2022-09-12 13:51:44 +0300

    suspended

--- test/app.js
+++ test/app.js
@@ -3,10 +3,10 @@ var zbc = require('./workers.js');
 async function main() {
   try {
     await zbc.deployWorkflow('./sample.bpmn')
-    const res = await zbc.createWorkflowInstanceWithResult('weather-report', {
-        requestTimeout: 2 * 60 * 1000
+    const res = await zbc.createWorkflowInstanceWithResult('process', {
+        requestTimeout: 10 * 60 * 1000
     });
-    console.log(JSON.stringify(res, null, 2))
+    // console.log(JSON.stringify(res, null, 2))
   } catch (e) {
     console.error(e)
   }
--- test/workers.js
+++ test/workers.js
@@ -4,16 +4,15 @@ const axios = require('axios')
 const zbc = new ZBClient();
 
 zbc.createWorker(null, "get-weather-report", async (job, complete) => {
-  // console.log(JSON.stringify(job, null, 2))
-  const city = job.variables.city
+    console.log("subprocess")
+    // console.log(JSON.stringify(job, null, 2))
   try {
       const res = await axios.get(`https://webayus-gateway.herokuapp.com/product/category`)
 
     //   console.log(res.data[0]);
       const deleted_code = res.data[0].isDeleted;
       complete.success({
-          deleted_code
-      })
+          deleted_code});
   } catch (e) {
       console.error("Something went wrong!")
       console.error(e)
@@ -22,15 +21,37 @@ zbc.createWorker(null, "get-weather-report", async (job, complete) => {
 });
 
 zbc.createWorker(null, "take-umbrella", (_, complete) => {
+    console.log("girmemesi lazım")
     complete.success({
         recommendation: "isDeleted is true!"
     })
 })
 
-zbc.createWorker(null, "leave-umbrella", (_, complete) => {
+zbc.createWorker(null, "leave-umbrella", (job, complete) => {
+    console.log("leave subprocess")
+    // console.log(JSON.stringify(job, null, 2))
     complete.success({
         recommendation: "isDeleted is false!"
     })
 })
+
+zbc.createWorker(null, "get-weather-report2", async (job, complete) => {
+    console.log("subprocess 2")
+    // console.log(JSON.stringify(job, null, 2))
+    const city = job.variables.city
+    try {
+        const res = await axios.get(`https://webayus-gateway.herokuapp.com/product/category`)
+  
+      //   console.log(res.data[0]);
+        const deleted_code = res.data[0].isDeleted;
+            complete.success({
+                deleted_code
+            })
+    } catch (e) {
+        console.error("Something went wrong!")
+        console.error(e)
+        complete.fail(e.message)
+    }
+  });
 
 module.exports = zbc;
